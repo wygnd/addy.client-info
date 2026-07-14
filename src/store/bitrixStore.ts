914389d1cd1153e62a65b22407cacc2069c9a560
Denis <wygndo@gmail.com>
2026-07-14 16:09:44 +0300

    fix: resize window

--- src/store/bitrixStore.ts
+++ src/store/bitrixStore.ts
@@ -51,7 +51,7 @@ export const useBitrixStore = defineStore("bitrix24", () => {
       await $logger.debug("Check options", options.value);
 
       // Масштабируем фрейм под размеры приложения
-      await $bx24.parent.resizeWindowAuto(null, 780, 1000);
+      await $bx24.parent.resizeWindowAuto(null, 900, 1000);
 
       await $logger.notice("Инициализация успешно выполнена");
       isInit.value = true;
