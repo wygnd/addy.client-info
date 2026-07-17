4d69f62c9599b158a1278851aedcb7f2c8cbf60e
Denis <wygndo@gmail.com>
2026-07-17 09:04:06 +0300

    fix: addyChatStore - api auth

--- src/store/addyChatStore.ts
+++ src/store/addyChatStore.ts
@@ -34,7 +34,7 @@ export const useAddyChatStore = defineStore("chats", () => {
       visitedPages.value.add(requestPage);
 
       const auth = btoa(
-        `${import.meta.env.VITE_BACKEND_API_USERNAME}:${import.meta.env.VITE_BACKEND_API_PASSWORD}`,
+        `${import.meta.env.VITE_ADDY_BACKEND_API_USERNAME}:${import.meta.env.VITE_ADDY_BACKEND_API_PASSWORD}`,
       );
       const { error, data } = await useApi<IApiAddyResponse<IAddyMessageResponse>>(
         `${API_ADDY_URL}/bx24/user/${clientStore.clientId}/messages?limit=${requestLimit}&page=${requestPage}`,
